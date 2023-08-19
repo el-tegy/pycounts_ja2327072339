85d9a8d3491f20416d661baed4544284c640dea9
el-tegy <elisee.tegue@groupe-esigelec.org>
2023-08-19 17:12:11 +0200

    fix: refactoring dirs structures

---
+++ src/pycounts/__init__.py
@@ -0,0 +1,4 @@
+"""Awesome `pycounts_ja2327072339` is a Python cli/package created with https://github.com/TezRomacH/python-package-template"""
+# read version from installed package
+from importlib.metadata import version
+__version__ = version("pycounts_ja2327072339")
